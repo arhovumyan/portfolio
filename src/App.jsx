298521f00f4a0c9aef8fadd5ba3b410d7089c337
Arego11 <aregar20012001@gmail.com>
2025-04-30 13:33:37 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+      <main>
+          <div>
+              <h1 className = "text-3xl underline">
+                  This is going to be my portfolio website
+              </h1>
+          </div>
+    </main>
+
+  )
+}
+
+export default App
