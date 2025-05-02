87e63d9a4339ed6eca0771c3e8c616b98fe49472
Arego11 <aregar20012001@gmail.com>
2025-05-02 13:19:51 -0700

    experience counter added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
 import { words } from "../constants/index.js";
 import Button from '../components/Button.jsx';
 import HeroExperience from '../components/HeroModels/HeroExperience.jsx';
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap'
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo('.hero-main-title',
+            {
+            y: 200,
+            opacity: 0
+            },
+            {
+                y: 0,
+                opacity: 1,
+                stagger: 0.2,
+                duration: 2,
+                ease: 'power2.inOut',
+            },
+        )
+        gsap.to('.hero-sub-title',
+            {
+                autoAlpha: 1,
+                duration: 2,
+                ease: 'power2.inOut',
+                delay: 1.5,
+            }
+        )
+        gsap.to('.hero-text hr',
+            {
+                autoAlpha: 1,
+                duration: 2,
+                ease: 'power2.inOut',
+                delay: 2,
+            }
+        )
+        gsap.to('.work-button', {
+            autoAlpha: 1,
+            duration: 2,
+            ease: 'power2.inOut',
+            delay: 2.5,
+        })
+        gsap.to('.hero-3d-layout', {
+            autoAlpha: 1,
+            duration: 2,
+            ease: 'power2.inOut',
+            delay: 2.5,
+        })
+    })
   return (
       <section id="hero" className="relative overflow-hidden">
           <div className='absolute top-0 left-0 z-10'>
@@ -14,7 +59,7 @@ const Hero = () => {
               <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
                   <div className='flex flex-col gap-7'>
                       <div className='hero-text'>
-                          <h1 className="transition-all duration-500 md:text-[60px] text-[40px] font-bold">
+                          <h1 className="hero-main-title md:text-[60px] text-[40px] font-bold">
                               We do
                             <span className="slide">
                               <span className="wrapper">
@@ -31,30 +76,32 @@ const Hero = () => {
                                   
                               </span>
                           </span> </h1>
-                          <hr className="mb-4 w-[650px] transition-all duration-500">
-                          </hr>
-                          <h1 className="mt-4 pointer-events-none">
+                          <hr className="opacity-0 mb-4 w-[400px] ml-50"/>
+                          <h1 className="hero-sub-title opacity-0 mt-4 pointer-events-none">
                               We are the Swiss army <br/>
                               knife for your business
                           </h1>
                       </div>
+                      {/*
                       <p className="transition-all duration-500">
                           Hello, my name is Areg, and I am the founder of Liner Inc.<br />
                           I have assembled a talented team of engineers who are dedicated to <br/>
                           helping you enhance and grow your business
                       </p>
-                      <Button className="md:w-80 md:h-16 w-60 h-12"
+                      */}
+                      <Button className="work-button opacity-0 md:w-80 md:h-16 w-60 h-12"
                           id="button"
                           text="See our works"
                       />
                   </div>
               </header>
               <figure>
-                <div className="hero-3d-layout">
-                    <HeroExperience />
+                <div className="hero-3d-layout opacity-0">
+                    <HeroExperience/>
                 </div>
               </figure>
           </div>
+          <AnimatedCounter/>
     </section>
   )
 }
