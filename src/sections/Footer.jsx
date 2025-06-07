07611210993c71bedce916125263d1e7f9f09c5a
Arego11 <aregar20012001@gmail.com>
2025-06-06 23:42:00 -0700

    resume changed

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className='logo footer'>
           <div className='footer-container'>
               <div className='footer-credentials desktop flex flex-col justify-center items-center'>
-                  <a href="/Areg Hovumyan Resume.pdf" target="_blank" rel="noopener noreferrer">See my resume</a>
+                  <a href="/Areg Hovumyan .pdf" target="_blank" rel="noopener noreferrer">See my resume</a>
               </div>
               <div className='socials'>
                   {socialImgs.map((img) => (
