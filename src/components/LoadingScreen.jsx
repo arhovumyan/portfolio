f22d9f3f97b4120fc6e0a773f380bb131af90bd7
Arego11 <aregar20012001@gmail.com>
2025-07-07 12:08:18 -0700

    changes

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 import './LoadingScreen.css';
 
 const LoadingScreen = () => {
   return (
     <div className="loading-screen">
-      <div className="balls-container">
-        <div className="ball"></div>
-        <div className="ball"></div>
-        <div className="ball"></div>
-      </div>
+      <DotLottieReact
+        src="https://lottie.host/e6b36637-56bc-4e05-9c93-70c4a1a125c7/8Q3ffIGUnE.lottie"
+        loop
+        autoplay
+      />
     </div>
   );
 };
